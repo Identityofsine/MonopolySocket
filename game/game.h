f86ddd610c6080d3b88ab6ccfe2b5d49f200b205
Identityofsine <67929513+Identityofsine@users.noreply.github.com>
2023-05-01 19:59:51 -0400

    Added function definitons for player movement

--- game/game.h
+++ game/game.h
@@ -4,12 +4,25 @@
 
 
 namespace Monopoly{
+
+
+    struct MonopolyRules {
+    public:
+        Money staringCash;
+
+    };
+
     struct MonopolyGame{
     private:
         unsigned int gameID;
+        Player* playerInTurn; // this is the player's turn, only listen to him.
         std::vector<Player*> players = std::vector<Player*>();
         Landable* spaces;
     public:
         MonopolyGame(unsigned int gameID);
+        bool addPlayer(Player* player);
+        bool startGame();
+        bool movePlayer(Player* player, int spaces);
+ 
     };
 }
